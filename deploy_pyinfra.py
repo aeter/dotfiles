4828b00506e465e59aa782425a71353010a2fb2e
A. Nackov <anackov@gmail.com>
2022-11-12 22:57:15 +0200

    add .profile block in .bashrc through pyinfra

--- deploy_pyinfra.py
+++ deploy_pyinfra.py
@@ -3,7 +3,7 @@
 # run with "pyinfra @local deploy_pyinfra.py"
 
 from pyinfra import host
-from pyinfra.operations import files
+from pyinfra.operations import files, server
 from pyinfra.facts.server import Home, Kernel
 
 HOMEDIR = host.get_fact(Home)
@@ -11,4 +11,6 @@ HOMEDIR = host.get_fact(Home)
 files.sync(src="./home", dest=HOMEDIR, exclude="*.profile")
 
 if host.get_fact(Kernel) == 'Linux':
-    files.put(src="./home/.profile", dest=f"{HOMEDIR}/.bashrc")
+    # files.block is currently in development in `pyinfra` - so we cat >> to append to .bashrc
+    files.put(src="./home/.profile", dest=f"{HOMEDIR}/.profile_dotfiles")
+    server.shell(commands=[f"cat {HOMEDIR}/.profile_dotfiles >> {HOMEDIR}/.bashrc"])
